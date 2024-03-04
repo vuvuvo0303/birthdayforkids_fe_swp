d32ea40d9ef9cc2afefd1b61057c8505692b9f4e
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-03-04 14:20:00 +0700

    Overflow for login form

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -48,8 +48,7 @@ export const LoginPage = () => {
     return (
         <Flex w="full" minH="100vh" justifyContent="center" alignItems="center" bgColor="#faebf7">
             <Flex minW="65%" h="80vh" justifyContent="flex-end" borderRadius="10px" shadow="0 0 10px gray" bg="url(https://images6.alphacoders.com/135/1350372.png)" bgSize="cover" bgPos="bottom">
-                <Box p="50px" bg="white" borderRadius="10px">
-                    <div></div>
+                <Box p="50px" bg="white" borderRadius="10px" overflow="auto">
                     <div>
                         <h1>Sign-In</h1>
                         <div>
