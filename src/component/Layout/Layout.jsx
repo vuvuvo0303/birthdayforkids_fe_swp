6a80ae5d34ebb272cd3b268a78d5973be428d85b
liulunlieule <lieucao16122003@gmail.com>
2024-03-28 18:00:43 +0700

    Updated

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -10,9 +10,16 @@ export const Layout = () => {
     const loggedUser = useSelector((store) => store.user);
     return (
         <>
-            {!loggedUser?.role && <Header />}
+            {(!loggedUser?.role || loggedUser?.role != "Guest" && loggedUser?.role === "Host") && <Header />}
             {loggedUser?.role === "Guest" && <HeaderLogin />}
             {loggedUser?.role === "Host" && <HeaderLoginOfHost />}
+            {/* if({loggedUser?.role === "Guest"}){
+                <HeaderLogin />
+            } else if ({loggedUser?.role === "Host"}){
+                <HeaderLoginOfHost />
+            } else {
+                <Header />
+            } */}
             <Outlet />
             <Footer />
         </>
