4e0cd868ebb3b8714fc80661efbf7f3420849b46
Your Name <you@example.com>
2024-03-28 17:32:14 +0700

    save

--- src/page/Wallet/Wallet.jsx
+++ src/page/Wallet/Wallet.jsx
@@ -6,127 +6,128 @@ import { WalletOutlined } from "@ant-design/icons";
 import { HeaderLogin } from "../../component/HeaderLogin";
 import { HeaderLoginOfHost } from "../../page/profile/HeaderLoginOfHost";
 import api from "../../config/axios";
-import { Table, Tag } from "antd";
+import { Table } from "antd";
 import { formatDistance } from "date-fns/formatDistance";
 import { useDispatch, useSelector } from "react-redux";
-import { CheckCircleOutlined, ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 
 export const Wallet = () => {
-  const [balance, setBalance] = useState(0);
-  const [transactions, setTransaction] = useState([]);
-  const [transactionHistory, setTransactionHistory] = useState([]);
-  const loggedUser = useSelector((store) => store.user);
+    const [balance, setBalance] = useState(0);
+    const [transactions, setTransaction] = useState([]);
+    const [transactionHistory, setTransactionHistory] = useState([]);
+    const loggedUser = useSelector((store) => store.user);
 
     const fetchWallet = async () => {
         const response = await api.get("/api/wallets");
-        // console.log("wallet", response.data);
         setBalance(response.data.totalMoney);
-        setWallet(response?.data);
-        // console.log("wallet", response?.data);
     };
-  const fetchWallet = async () => {
-    const response = await api.get("/api/wallets");
-    setBalance(response.data.totalMoney);
-  };
 
+    const fetchTransaction = async () => {
+        const response = await api.get("/api/transactions");
+        setTransaction(
+            response.data.sort(
+                (item1, item2) =>
+                    new Date(item2.createAt) - new Date(item1.createAt)
+            )
+        );
+    };
 
-  const fetchTransaction = async () => {
-    const response = await api.get("/api/transactions");
-    setTransaction(response.data.sort((item1, item2) => new Date(item2.createAt) - new Date(item1.createAt)));
-  };
-
-    
     useEffect(() => {
-        fetchTransaction(wallet?.walletID);
-        // loggedUser?.accountID
-    }, [wallet]);
-  useEffect(() => {
-    fetchWallet();
-    fetchTransaction();
-  }, []);
+        fetchWallet();
+        fetchTransaction();
+    }, []);
 
-  const addMoney = () => {
-    const amount = parseFloat(prompt("Enter the amount to add:"));
+    const addMoney = () => {
+        const amount = parseFloat(prompt("Enter the amount to add:"));
 
-    if (isNaN(amount) || amount <= 0) {
-      alert("Invalid amount. Please enter a valid number greater than zero.");
-      return;
-    } else {
-    }
+        if (isNaN(amount) || amount <= 0) {
+            alert(
+                "Invalid amount. Please enter a valid number greater than zero."
+            );
+            return;
+        } else {
+        }
 
-    const updatedBalance = balance + amount;
-    setBalance(updatedBalance);
-    addTransaction(`Added $${amount.toFixed(2)}`);
-  };
+        const updatedBalance = balance + amount;
+        setBalance(updatedBalance);
+        addTransaction(`Added $${amount.toFixed(2)}`);
+    };
 
-  const addTransaction = (description) => {
-    setTransactionHistory([...transactionHistory, description]);
-  };
+    const addTransaction = (description) => {
+        setTransactionHistory([...transactionHistory, description]);
+    };
 
-  const columns = [
-    {
-      title: "Order Id",
-      dataIndex: "order",
-      key: "order",
-      render: (value) => {
-        return value.orderID;
-      },
-    },
-    {
-      title: "Account",
-      dataIndex: "order",
-      key: "order",
-      render: (value) => value.customerName,
-    },
-    
-    {
-        title: "Value",
-        dataIndex: "order",
-        key: "order",
-        render: (value) => value.totalPrice.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }),
-    },
-    
-    {
-      title: "Create At",
-      dataIndex: "createAt",
-      key: "createAt",
-      render: (value) =>
-        formatDistance(new Date(value), new Date(), {
-          addSuffix: true,
-        }),
-    },
-  ];
-  return (
-    <div>
-      {loggedUser?.role === "Guest" && <HeaderLogin />}
-      {loggedUser?.role === "Host" && <HeaderLoginOfHost />}
-      <div className="wallet-container">
-        <div className="wallet-first">
-          <div className="wallet-header">
-            <WalletOutlined />
-            <h2>Your Wallet</h2>
-          </div>
-          <div className="wallet-balance">
-            Balance: {balance.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
-          </div>
+    const columns = [
+        {
+            title: "Order Id",
+            dataIndex: "order",
+            key: "order",
+            render: (value) => {
+                return value.orderID;
+            },
+        },
+        {
+            title: "Account",
+            dataIndex: "order",
+            key: "order",
+            render: (value) => value.customerName,
+        },
 
-          <div className="wallet-actions">
-            <button className="wallet-button" onClick={addMoney}>
-              Add Money
-            </button>
-            <button className="wallet-button">Withdraw</button>
-          </div>
-        </div>
-        <div className="transaction-history">
-          <h3>Transaction History</h3>
-          <Table dataSource={transactions} columns={columns} />
-          {/* <ul>
+        {
+            title: "Value",
+            dataIndex: "order",
+            key: "order",
+            render: (value) =>
+                value.totalPrice.toLocaleString("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                }),
+        },
+
+        {
+            title: "Create At",
+            dataIndex: "createAt",
+            key: "createAt",
+            render: (value) =>
+                formatDistance(new Date(value), new Date(), {
+                    addSuffix: true,
+                }),
+        },
+    ];
+    return (
+        <div>
+            {loggedUser?.role === "Guest" && <HeaderLogin />}
+            {loggedUser?.role === "Host" && <HeaderLoginOfHost />}
+            <div className="wallet-container">
+                <div className="wallet-first">
+                    <div className="wallet-header">
+                        <WalletOutlined />
+                        <h2>Your Wallet</h2>
+                    </div>
+                    <div className="wallet-balance">
+                        Balance:{" "}
+                        {balance.toLocaleString("vi-VN", {
+                            style: "currency",
+                            currency: "VND",
+                        })}
+                    </div>
+
+                    <div className="wallet-actions">
+                        <button className="wallet-button" onClick={addMoney}>
+                            Add Money
+                        </button>
+                        <button className="wallet-button">Withdraw</button>
+                    </div>
+                </div>
+                <div className="transaction-history">
+                    <h3>Transaction History</h3>
+                    <Table dataSource={transactions} columns={columns} />
+                    {/* <ul>
                         {transactions.map((transaction, index) => (
                             <li key={index}>{transaction.transactionID}</li>
                         ))}
                     </ul> */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
